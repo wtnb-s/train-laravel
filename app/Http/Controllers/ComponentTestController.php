52b37aef34eb57b7e836f06add2b389e63fcc628
wtnb-s <denebu.1.4@gmail.com>
2021-12-26 18:58:31 +0900

    コンポーネント追加

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -7,11 +7,17 @@ use Illuminate\Http\Request;
 class ComponentTestController extends Controller
 {
     //
-    public function showComponent1(){
-        return view('tests.component-test1');
+    public function showComponent1()
+    {
+        $message = 'メッセージ';
+        return view(
+            'tests.component-test1',
+            compact('message')
+        );
     }
-    
-    public function showComponent2(){
+
+    public function showComponent2()
+    {
         return view('tests.component-test2');
     }
 }
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,1 +1,4 @@
-テストコンポーネント２
+<x-tests.app>
+    <x-slot name="header">ヘッダー２</x-slot>
+    テストコンポーネント2
+</x-tests.app>
