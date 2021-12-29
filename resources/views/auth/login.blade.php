c8918417d6e022f27caddc308fbd0243c037af23
wtnb-s <denebu.1.4@gmail.com>
2021-12-29 12:46:05 +0900

    クラスベースのコンポーネント追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,10 +26,7 @@
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
@@ -42,9 +39,9 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
                 @endif
 
                 <x-button class="ml-3">
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,8 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー２</x-slot>
     テストコンポーネント2
+    <x-test-class-base classBaseMessage="メッセージです" />
+    <div class="mb-4">
+        <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更" />
+    </div>
 </x-tests.app>
