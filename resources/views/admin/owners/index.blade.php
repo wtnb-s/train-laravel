22c87d50b984502c9ad45513504d6f3a92316b01
wtnb-s <denebu.1.4@gmail.com>
2022-01-30 21:59:12 +0900

    ページネーション追加

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -51,6 +51,7 @@
                                         @endforeach
                                     </tbody>
                                 </table>
+                                {{ $owners->links() }}
                             </div>
                         </div>
                     </section>
