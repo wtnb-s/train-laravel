472933eccaee63ddcc11118fb1072d69cb470e1f
wtnb-s <denebu.1.4@gmail.com>
2022-02-27 22:55:58 +0900

    ショップページ追加

--- routes/admin.php
+++ routes/admin.php
@@ -30,14 +30,8 @@ Route::resource('owners', OwnersController::class)
     ->middleware(['auth:admin'])->except(['show']);
 
 Route::prefix('expired-owners')->middleware('auth:admin')->group(function () {
-    Route::get('index', [
-        OwnersController::class,
-        'expiredOwnerIndex'
-    ])->name('expired-owners.index');
-    Route::post('destroy/{owner}', [
-        OwnersController::class,
-        'expiredOwnerDestroy'
-    ])->name('expired-owners.destroy');
+    Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])->name('expired-owners.index');
+    Route::post('destroy/{owner}', [OwnersController::class, 'expiredOwnerDestroy'])->name('expired-owners.destroy');
 });
 
 Route::get('/dashboard', function () {
